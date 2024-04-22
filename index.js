ccbd91e19d8efe6aeaea864eda68671b539bac9d
Sunil Gollapalli <82632456+Sunil1919-tech@users.noreply.github.com>
2024-04-22 14:50:38 +0530

    Simple JS code challenge

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("connected")
+
+
+// celecius to Fahrenheit
+
+let celecius=34;
+let farenheit= (celecius*1.8) + 32;
+console.log("The temperature in Fahrenheit is  " +farenheit)
+
+
+// Fahrenheit to Celcius
+
+let fahrenheit2= 45;
+let celsius2 =  (fahrenheit2 -32) * 0.5556;
+console.log("The Temperature in Celcius  " +celsius2)
